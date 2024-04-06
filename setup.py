b20df426147e636abcb45e43f02a1e0e41853f14
Katalin Ferenc <35810660+ferenckata@users.noreply.github.com>
2024-04-06 15:43:16 +0200

    Update setup.py with 1.0.0 version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
   name='CAVACHON',
-  version='0.0.0',
+  version='1.0.0',
   description="""Cell cluster Analysis with Variational Autoencoder using Conditional Hierarchy Of latent representioN""",
   url='https://github.com/dn070017/CAVACHONE',
   author='Ping-Han Hsieh',
